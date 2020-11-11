6772ec7259b4fbef1a903d93738bb8a3951d6afa
BenjaminKing1337 <BenjaminKing1337@users.noreply.github.com>
2020-11-12 00:27:59 +0100

    about
    samo and benji

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,57 @@
 <template>
   <div class="about">
     <v-layout>
-      <v-card class="mx-auto my-10 pa-3">
-        
-        <h1> This is an about header.</h1>
-        <p>This is an about text</p>
-        <v-card>
-          <v-col><v-img>
-            </v-img></v-col>
-          <v-col><v-img>
-            </v-img></v-col>
+      <v-flex>
+        <v-card class="mx-auto my-10 pa-9" width="90%">
+          <h1>This is an about header.</h1>
+          <hr />
+          <p>This is an about text</p>
+
+          <v-row>
+            <v-col id="symbols">
+              <v-img src="@/assets/logo.png"> </v-img>
+              <v-img src="@/assets/logo.png"> </v-img>
+              <v-img src="@/assets/logo.png"> </v-img>
+              <v-img src="@/assets/logo.png"> </v-img>
+            </v-col>
+          </v-row>
         </v-card>
-      </v-card>
+        <v-card
+          id="sam"
+          class="mx-auto my-10 pa-9"
+          width="90%"
+          color="transparent"
+        >
+          <v-row id="creators">
+            <v-col>
+              <v-img src="@/assets/samo.png"> </v-img>
+            </v-col>
+            <v-col id="usText">
+              <h1>This is a Sam.</h1>
+              <hr />
+              <p>This is about Sam</p>
+            </v-col>
+          </v-row>
+        </v-card>
+        <v-card
+          id="ben"
+          class="mx-auto my-10 pa-9"
+          width="90%"
+          color="transparent"
+        >
+          <v-row class="creators">
+            
+            <v-col id="usText">
+              <h1>This is a Benji.</h1>
+              <hr />
+              <p>This is about Benji</p>
+            </v-col>
+            <v-col>
+              <v-img src="@/assets/ben.png"> </v-img>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-flex>
     </v-layout>
   </div>
 </template>
@@ -22,3 +62,35 @@ export default {
   components: {},
 };
 </script>
+
+<style lang="scss" scoped>
+@import "./src/styles/variables.scss";
+
+#symbols {
+  display: flex;
+  flex-flow: row wrap;
+}
+
+#ben,
+#sam {
+  box-shadow: none;
+  #usText {
+    background-color:map-get($Colorscheme, denary);
+    border-radius: 5px;
+    margin: auto 0;
+    padding: 30px;
+  }
+}
+
+
+@media only screen and (max-width: 850px) {
+  #creators {
+    display: flex;
+    flex-direction: row;
+  }
+  .creators {
+    display: flex;
+    flex-direction: column-reverse;
+  }
+}
+</style>
