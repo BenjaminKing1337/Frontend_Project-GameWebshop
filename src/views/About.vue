d95eeaab6232ef53f42c126f355b8478dc812993
BenjaminKing1337 <BenjaminKing1337@users.noreply.github.com>
2020-11-11 20:47:46 +0100

    a

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,15 @@
   <div class="about">
     <v-layout>
       <v-card class="mx-auto my-10 pa-3">
-        This is an about page.
+        
+        <h1> This is an about header.</h1>
+        <p>This is an about text</p>
+        <v-card>
+          <v-col><v-img>
+            </v-img></v-col>
+          <v-col><v-img>
+            </v-img></v-col>
+        </v-card>
       </v-card>
     </v-layout>
   </div>
